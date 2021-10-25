a90b8927b72916abc79aa70fe032d97f361c430d
timoflask <88914566+timoflask@users.noreply.github.com>
2021-10-25 07:23:55 +0300

    intro section responsive

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,11 +3,13 @@ import video from '../../assets/videos/HeroVid.mp4';
 
 export const Intro = () => {
     return (
-        <section className="intro">
-            <video autoPlay loop muted>
-                <source src={video} type="video/mp4"/>
-            </video>
-        </section>
+        <div className="intro">
+            <div className="intro__video">
+                <video autoPlay loop muted>
+                    <source src={video} type="video/mp4"/>
+                </video>
+            </div>
+        </div>
     )
 }
 
